91d52d259acd7cbfe12ad9c9ece3061d828f5cab
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-09-23 01:30:10 +0200

    Finish the Game Logic

--- components/screens/GameOverScreen.js
+++ components/screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, Button } from 'react-native';
 
 const GameOverScreen = props => {
     return (
     <View style={styles.screen}>
         <Text>The Game is Over !</Text>
+    <Text>Number of rounds: {props.roundsNumber}</Text>
+    <Text>Number was: {props.userNumber}</Text>
+    <Button title="NEW GAME" onPress={props.onRestart}/>
     </View>);
 }
 const styles = StyleSheet.create({
