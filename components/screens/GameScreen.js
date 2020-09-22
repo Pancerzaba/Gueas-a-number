42a4d85aaf310683cab0bec423b275d820850001
Pancerzaba <marta.rozmyslowicz21@gmail.com>
2020-09-23 01:13:06 +0200

    Add finish Game

--- components/screens/GameScreen.js
+++ components/screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Button, Alert } from 'react-native';
 import NumberContainer from '../NumberContainer';
 import Card from '../Card';
@@ -19,8 +19,17 @@ const GameScreen = props => {
         generateRandomBetween(1, 100, props.userChoice)
     );
 
+    const [rounds, setRounds]=useState(0);
     const currentLow = useRef(1);
     const currentHight = useRef(100);
+
+    const {userChoice, onGameOver} = props;
+
+    useEffect(() => {
+        if(currentGuess === props.userChoice){
+            props.onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver])
 
     const nextGuessHandler = direction => {
         if((direction=== 'lower' && currentGuess < props.userChoice) ||(direction === 'greater' && currentGuess > props.userChoice)){
@@ -34,6 +43,7 @@ const GameScreen = props => {
         }
         const nextNumber = generateRandomBetween(currentLow.current, currentHight.current, currentGuess);
         setCurrentGuess(nextNumber);
+        setRounds(curRounds => curRounds + 1)
     }
 
     return (
